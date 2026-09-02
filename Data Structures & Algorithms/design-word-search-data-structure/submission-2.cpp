10e17bf9c8895815b94afab789a162262166b9ca
MJaithra <137266209+MJaithra@users.noreply.github.com>
2026-09-02 22:06:53 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.cpp
@@ -0,0 +1,67 @@
+class WordDictionary {
+public:
+    struct TrieNode{
+        bool isEnd;
+        TrieNode* next[26];
+
+        TrieNode(){
+            isEnd = false;
+            for(int i=0; i<26; i++)
+            {
+                next[i] = nullptr;
+            }
+        }
+    };
+
+    TrieNode* root;
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* curr = root;
+        for(char c : word)
+        {
+            if(!curr->next[c-'a'])
+            {
+                curr->next[c-'a'] = new TrieNode();
+            }
+
+            curr = curr->next[c-'a'];
+        }
+        curr->isEnd = true;
+    }
+    
+    bool backtrack(int ind, string &word, TrieNode* curr)
+    {
+        if(ind == word.length())
+        {
+            return curr->isEnd;
+        }
+
+        char c = word[ind];
+        if(c != '.')
+        {
+            if(curr->next[c-'a'])
+                return backtrack(ind+1, word, curr->next[c-'a']);
+            
+            else
+                return false;
+        }
+       
+        for(int i=0; i<26; i++)
+        {
+            if(curr->next[i])
+            {
+                if(backtrack(ind+1, word, curr->next[i]))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+    bool search(string word) {
+        TrieNode* curr = root;
+        return backtrack(0, word, curr);
+    }
+};
