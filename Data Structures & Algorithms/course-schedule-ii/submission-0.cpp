187cf7a2fff306a47487aced958c4aa9c311659f
MJaithra <137266209+MJaithra@users.noreply.github.com>
2026-09-05 21:49:11 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indeg(numCourses, 0);
+        int m = prerequisites.size();
+        for(int i=0; i<m; i++)
+        {
+            indeg[prerequisites[i][0]]+=1;
+        }
+
+        queue<int> q;
+        for(int i=0; i< numCourses; i++)
+        {
+            if(indeg[i] == 0)
+                q.push(i);
+        }
+
+        vector<int> order;
+        while(!q.empty())
+        {
+            int n = q.front();
+            q.pop();
+
+            order.push_back(n);
+            for(int i=0; i<m; i++)
+            {
+                if(prerequisites[i][1] == n)
+                {
+                    indeg[prerequisites[i][0]]--;
+                    if(indeg[prerequisites[i][0]] == 0)
+                        q.push(prerequisites[i][0]);
+                }
+                
+            }
+        }
+
+        return order.size() == numCourses ? order : vector<int>{};
+    }
+};
